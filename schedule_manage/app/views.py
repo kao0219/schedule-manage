c7f23bf02fca6b826bf6e59a0867459aeedf42b5
kao0219 <m9r4.18@gmail.com>
2025-03-28 23:44:07 +0900

    def login_view追加

--- schedule_manage/app/views.py
+++ schedule_manage/app/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('<h1>schedule manage</h1>')
+
+
+def login_view(request):
+    return render(request, 'login.html')
