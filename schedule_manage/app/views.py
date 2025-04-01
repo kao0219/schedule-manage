95de328b21dbfdb230d4d3f6ce8a55c6c2a92133
kao0219 <m9r4.18@gmail.com>
2025-04-01 21:45:13 +0900

    viewsにアドレスログイン対応

--- schedule_manage/app/views.py
+++ schedule_manage/app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, get_user_model
+from django.contrib import messages 
 
 def index(request):
     return HttpResponse('<h1>schedule manage</h1>')
