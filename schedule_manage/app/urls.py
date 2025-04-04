354dd545640b1f84eea346765048e336c2cac9eb
kao0219 <m9r4.18@gmail.com>
2025-04-04 11:41:04 +0900

    urlsにhomeのpath追加

--- schedule_manage/app/urls.py
+++ schedule_manage/app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
+    path('home/', views.home_view, name='home'),
 ]
