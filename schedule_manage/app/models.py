023481d784bf2640150f9a216922331a25cc5cfa
kao0219 <m9r4.18@gmail.com>
2025-04-04 10:40:04 +0900

    models.pyにログイン処理の部分入力

--- schedule_manage/app/models.py
+++ schedule_manage/app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+#ユーザーの管理用のマネージャー
+class CustomUserManager(BaseUserManager):
+    def create_user(self, emali, password=None, username=None):
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, username=None):
+        user = self.create_user(email=email, passord=password, username=username)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
