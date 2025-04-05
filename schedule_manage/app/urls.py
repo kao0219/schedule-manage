02e6abc9441fbd19c9419fbf07859405737442c2
kao0219 <m9r4.18@gmail.com>
2025-04-05 21:37:02 +0900

    urlsにお気に入り予定のpath入力

--- schedule_manage/app/urls.py
+++ schedule_manage/app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('home/', views.home_view, name='home'),
     path('memos/', views.memos_view, name='memos'),
+    path('favorites/schedule/', views.favorites_schedule_view, name='favorites_schedule'),
     path('favorites/', views.favorites_view, name='favorites'),
     path('settings/', views.settings_view, name='settings'),
     path('logout/', views.logout_view, name='logout'),
