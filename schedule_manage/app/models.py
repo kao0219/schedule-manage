966b0914caf3465cd7f48aedfa03afe9d070f413
kao0219 <m9r4.18@gmail.com>
2025-04-06 17:27:38 +0900

    modelsにtimezoneとget_random_stringを入力

--- schedule_manage/app/models.py
+++ schedule_manage/app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.utils import timezone
+from django.utils.crypto import get_random_string
 
 #ユーザーの管理用のマネージャー
 class CustomUserManager(BaseUserManager):
