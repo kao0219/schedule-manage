36d391a0ce7f8f9cc07be62bc2f2020ac405c7da
kao0219 <m9r4.18@gmail.com>
2025-04-06 15:27:52 +0900

    viewsのobj.email→obj.usernameに変更

--- schedule_manage/app/views.py
+++ schedule_manage/app/views.py
@@ -27,7 +27,7 @@ def login_view(request):
         try:
             #カスタムユーザーからメールで探す
             user_obj = User.objects.get(email=email)
-            user = authenticate(request, username=user_obj.email, password=password) #username→emailで認証
+            user = authenticate(request, username=user_obj.username, password=password) #username→ユーザー名で認証
 
             if user is not None:
                 print("ログイン成功")
@@ -97,7 +97,7 @@ def change_password_view(request):
         if form.is_valid():
             form.save()
             update_session_auth_hash(request, request.user)
-            return redirect('app:settings')
+            return redirect('app:home')
     else:
         form = CustomPasswordChangeForm(user=request.user)
 
