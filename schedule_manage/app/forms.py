2d612265d26692287eff3b75b80e21d8d249fe7f
kao0219 <m9r4.18@gmail.com>
2025-04-06 16:15:15 +0900

    formsにアドレス変更部分入力

--- schedule_manage/app/forms.py
+++ schedule_manage/app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import PasswordChangeForm
 
+
 class CustomPasswordChangeForm(PasswordChangeForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -11,3 +12,18 @@ class CustomPasswordChangeForm(PasswordChangeForm):
 
         for field in self.fields.values():
             field.help_text =''
+
+
+class CustomEmailChangeForm(forms.Form):
+    current_email = forms.EmailField(label='現在のメールアドレス')
+    new_email = forms.EmailField(label='新しいメールアドレス')
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+
+    def clean_current_email(self):
+        current_email = self.cleaned_data['current_email']
+        if self.user and current_email !=self.user.email:
+            raise forms.ValidationError('')
+        return current_email
