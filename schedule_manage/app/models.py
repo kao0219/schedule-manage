6fc80b72f8fbd7a082a7236a21f2430896d49a0e
kao0219 <m9r4.18@gmail.com>
2025-04-07 14:28:24 +0900

    modelsにhas_permとhas_module_perms追加

--- schedule_manage/app/models.py
+++ schedule_manage/app/models.py
@@ -40,6 +40,12 @@ class CustomUser(AbstractBaseUser):
 
     def __str__(self):
         return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+    
+    def has_module_perms(self, app_label):
+        return self.is_superuser
 
 class Invite(models.Model):
     invite_token = models.CharField(max_length=255, unique=True, editable=False)
