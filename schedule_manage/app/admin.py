f99a26a1d6a9f4a4097e269a383e8f2441b55070
kao0219 <m9r4.18@gmail.com>
2025-04-13 20:10:50 +0900

    admin.pyに管理画面に表示させるもの入力

--- schedule_manage/app/admin.py
+++ schedule_manage/app/admin.py
@@ -2,9 +2,17 @@ from django.contrib import admin
 from .models import Invite
 from .models import Family
 admin.site.register(Family)
+from .models import CustomUser, Family
 
 @admin.register(Invite)
 class InviteAdmin(admin.ModelAdmin):
     list_display = ('invite_token', 'status', 'expires_at', 'create_at', 'update_at')
     list_filter = ('status',)
     search_fields = ('invite_token',)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'family')
+    fields = ('email', 'username', 'family', 'is_active', 'is_staff', 'is_superuser')
+
+admin.site.register(Family)
