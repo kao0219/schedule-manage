8c3ae5c5a3d9a57d96861d701e40f1155a129693
kao0219 <m9r4.18@gmail.com>
2025-04-13 16:42:57 +0900

    modelsにfamily部分入力

--- schedule_manage/app/models.py
+++ schedule_manage/app/models.py
@@ -56,3 +56,10 @@ class Invite(models.Model):
 
     def is_expired(self):
         return timezone.now() > self.expires_at
+
+class Family(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return F"Family {self.id}"
