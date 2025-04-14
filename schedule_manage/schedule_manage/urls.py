af76a33e1be971a696aaee79501de568fa26642f
kao0219 <m9r4.18@gmail.com>
2025-04-14 20:41:11 +0900

    app/urlsにsearchを追加

--- schedule_manage/schedule_manage/urls.py
+++ schedule_manage/schedule_manage/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
+    path('', views.index, name='index'),
+    path('search/', views.seach_view, name='search'),
 
 ]
