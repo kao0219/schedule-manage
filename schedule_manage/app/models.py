6c9a8b533f0f5be5e44d8773f012e328297d1f61
kao0219 <m9r4.18@gmail.com>
2025-04-24 22:16:03 +0900

    modelsのmemo編集

--- schedule_manage/app/models.py
+++ schedule_manage/app/models.py
@@ -118,7 +118,7 @@ class ScheduleComment(models.Model):
 class Memo(models.Model):
     memo_title = models.CharField(max_length=200)
     content = models.TextField()
-    image_url = models.CharField(max_length=500, blank=True, null=True)  # 画像のURLを保存
+    image = models.ImageField(upload_to='memo_images/', blank=True, null=True)  # 画像添付
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
