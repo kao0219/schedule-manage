6d0312ce93a0a6cdc97a5597bd3f787d0baccee7
kao0219 <m9r4.18@gmail.com>
2025-04-29 00:47:48 +0900

    viewsにrequire_post追加

--- schedule_manage/app/views.py
+++ schedule_manage/app/views.py
@@ -27,6 +27,7 @@ from django.core.paginator import Paginator
 from datetime import datetime
 import json
 from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 User = get_user_model()
 
