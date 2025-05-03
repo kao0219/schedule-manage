0e28c33a6ab6b3315918b456af6364dd962a1f40
kao0219 <m9r4.18@gmail.com>
2025-05-03 23:10:42 +0900

    commentstatus削除

--- schedule_manage/app/models.py
+++ schedule_manage/app/models.py
@@ -110,7 +110,6 @@ class ScheduleComment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
     content = models.CharField(max_length=255, verbose_name='コメントの内容')  
-    comment_status = models.IntegerField(default=1)  # 1:未読、2:既読
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
