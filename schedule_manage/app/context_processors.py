f45ea4e5b48bc6fbaa99817688db77dfb734a356
kao0219 <m9r4.18@gmail.com>
2025-05-04 20:20:41 +0900

    コメントマーク表示の部分入力

--- schedule_manage/app/context_processors.py
+++ schedule_manage/app/context_processors.py
@@ -1,1 +1,12 @@
-from .models import ScheduleComment, ScheduleCommentRead
+from .models import ScheduleComment, ScheduleCommentRead #予定コメントマーク表示のため作成
+
+def unread_comment_flag(request):
+    if request.user.is_authenticated:
+        read_ids = ScheduleCommentRead.objects.filter(user=request.user).values_list('comment_id', flat=True)
+        has_unread = ScheduleComment.objects.exclude(id__in=read_ids).exists()
+    else:
+        has_unread = False
+
+    return {
+        'has_unread_comment': has_unread
+    }
