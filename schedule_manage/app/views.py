1aa91c53e9469c6194b780e2f19845e1279eecd4
Kaori　Suzuki <m9r4.18@gmail.com>
2025-06-03 21:40:05 +0900

    views最新状態UP

--- schedule_manage/app/views.py
+++ schedule_manage/app/views.py
@@ -422,7 +422,7 @@ def schedule_detail_view(request, schedule_id):
 
     comments = ScheduleComment.objects.filter(
         schedule=schedule,
-        # display_date=filter_date    #←これを入れるとコメント保存されても予定ページに表示されない
+        # display_date=filter_date   #←これを入れるとコメント保存されても予定ページに表示されない
     ).order_by('-created_at')
     print("表示対象のコメント数 =", comments.count())
     print("コメント取得用 date_obj:", date_obj)
