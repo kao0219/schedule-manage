c5d510b469b649e84f7464fe7c533fd09e8a989f
Kaori　Suzuki <m9r4.18@gmail.com>
2025-06-15 14:34:38 +0900

    アプリ名入力

--- schedule_manage/schedule_manage/urls.py
+++ schedule_manage/schedule_manage/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    path('schedule_manage/', include('app.urls')),
 
 ]
 if settings.DEBUG:
