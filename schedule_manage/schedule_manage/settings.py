ccc5c7f57d529e5afeeef1e7de37c827791f99ef
Kaori　Suzuki <m9r4.18@gmail.com>
2025-07-23 23:16:09 +0900

    false

--- schedule_manage/schedule_manage/settings.py
+++ schedule_manage/schedule_manage/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wl@&g7((c47)com3papcm87rcr3xbsvls^@l1hrw!14_z&k4bd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
-
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','kaorisuzuki.pythonanywhere.com']
 
